a4db4539e27f0df0210edbb50c238d9a2c7676e7
See-Mong Tan <seemong@gmail.com>
2018-03-24 15:59:20 -0700

    Pygame display

--- display/display.py
+++ display/display.py
@@ -1,7 +1,17 @@
 from __future__ import print_function
+import pygame
 
 def main():
     print("Hello World")
+
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 1000))
+
+    pygame.display.flip()
+    while True:
+        event == pygame.event.wait()
+        if event.type == pygame.QUIT:
+            break
 
 if __name__ == "__main__":
     main()
