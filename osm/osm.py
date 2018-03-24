23645b5c0a04ab85ce0a5f5dd06d89733f8b4c20
See-Mong Tan <seemong@gmail.com>
2018-03-24 14:09:32 -0700

    filtering for way nodes

--- osm/osm.py
+++ osm/osm.py
@@ -1,17 +1,21 @@
 from __future__ import print_function
 import sys
 import xml.etree.ElementTree as etree
+import time
 
 def main():
     print('Hello OSM')
+    count = 0
     
-    spinner = ['/', '-', '\\', '|']
-    count = 0;
-
     xml = open("washington-latest.osm", "r")
     for event, elem in etree.iterparse(xml):
+        count += 1
+
         if elem.tag == 'way':
             print(elem.tag, elem.attrib)
+
+    print('Parsed {0} elements'.format(count))
+
 
 
 if __name__ == "__main__":
