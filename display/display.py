633d037319a19ea241a71c7a8af5bd4d17dca99a
See-Mong Tan <seemong@gmail.com>
2018-03-26 18:45:31 -0700

    OpenGL movement

--- display/display.py
+++ display/display.py
@@ -1,21 +1,64 @@
 from __future__ import print_function
 import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices = (
+    (0, 1, 0), (-1, 0, 0), (0, -1, 0), (1, 0, 0)
+)
+
+edges = (
+    (0, 1), (1, 2), (2, 3), (3, 0)
+)
+
+def draw_diamond():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
 
 def main():
     print("Hello World")
 
     pygame.init()
-    screen = pygame.display.set_mode((800, 800))
+    display = (800, 800)
+    screen = pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     
-    red = (255, 0, 0)
+    gluPerspective(45, 1, 0.1, 10)
     
-    pygame.draw.lines(screen, red, True, [(100, 100), (50, 300), (12, 13)], 1)
-
-    pygame.display.flip()
+    x = 0.0
+    y = 0.0
+    z = -5.0
+    glTranslatef(x, y, z)
+ 
     while True:
+        delta_x = delta_y = delta_z = 0
+        
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
             break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                delta_x = -0.1
+            if event.key == pygame.K_LEFT:
+                delta_x = 0.1
+            if event.key == pygame.K_UP:
+                delta_y = -0.1
+            if event.key == pygame.K_DOWN:
+                delta_y = 0.1
+            if event.key == pygame.K_q:
+                delta_z = 0.1
+            if event.key == pygame.K_a:
+                delta_z = -0.1
+                
+        glTranslatef(delta_x, delta_y, delta_z)
+            
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        draw_diamond()
+        pygame.display.flip()
+        pygame.time.wait(10)
 
     pygame.display.quit()
     pygame.quit()
