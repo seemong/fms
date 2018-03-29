9600eb7a19c6c6668940a79beb4988c538477d6f
See-Mong Tan <seemong@gmail.com>
2018-03-29 14:25:36 -0700

    Finally got lookAt working

--- display/display.py
+++ display/display.py
@@ -3,10 +3,12 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import math
 
 vertices = (
-    (0, 2, 0), (-2, 1, 0), (-1, 1, 0), (-1, 0, 0),
-    (1, 0, 0), (1, 1, 0), (2, 1, 0)
+    (0, 3, 0), (-2, 2, 0), (-1, 2, 0), (-1, 0, 0),
+    (1, 0, 0), (1, 2, 0), (2, 2, 0)
 )
 
 edges = (
@@ -19,6 +21,20 @@ def draw_diamond():
         for vertex in edge:
             glVertex3fv(vertices[vertex])
     glEnd()
+    
+def draw_floor():
+    glBegin(GL_LINES)
+    for x in range(-10, 11):
+        glVertex3fv((x, 0, 10))
+        glVertex3fv((x, 0, -10))
+        
+    for z in range(-10, 11):
+        glVertex3fv((10, 0, z))
+        glVertex3fv((-10, 0, z))
+    glEnd()
+    
+
+
 
 def main():
     print("Hello World")
@@ -27,13 +43,19 @@ def main():
     display = (800, 800)
     screen = pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     
-    gluPerspective(45, 1, 0.1, 10)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(90, 1, 0.1, 100)
     
     x = 0.0
     y = 0.0
-    z = -5.0
-    glTranslatef(x, y, z)
+    z = 5.0
+    # glTranslatef(x, y, z)
+    #print_mv()
+    # gluLookAt(0, 3, 5, 0, 0, 0, 0, 1, 0)
+    #print_mv()
  
+    rotation_direction = 0
     quit = False
     while not quit:
         delta_x = delta_y = delta_z = 0
@@ -46,23 +68,21 @@ def main():
                 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    delta_x = -0.1
+                    rotation_direction = 1
                 if event.key == pygame.K_LEFT:
-                    delta_x = 0.1
-                if event.key == pygame.K_UP:
-                    delta_y = -0.1
-                if event.key == pygame.K_DOWN:
-                    delta_y = 0.1
-                if event.key == pygame.K_q:
-                    delta_z = 0.1
-                if event.key == pygame.K_a:
-                    delta_z = -0.1
-                    
-        glTranslatef(delta_x, delta_y, delta_z)
-        glRotatef(1, 0, 1, 1)
-            
+                    rotation_direction = -1
+        
+        # glRotatef(rotation_direction, 0, 0, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(0, y, z, 0, 0, 0, 0, 1, 0)
+        z += 0.1
+        y += 0.1
+ 
         draw_diamond()
+        draw_floor()
+
         pygame.display.flip()
         pygame.time.wait(10)
 
