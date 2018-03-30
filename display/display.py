bbd35a2d3b7e439c0e55970fe615d7dda2832e00
See-Mong Tan <seemong@gmail.com>
2018-03-29 23:02:05 -0700

    glut working

--- display/display.py
+++ display/display.py
@@ -31,7 +31,7 @@ colors = (
 )
 
 def draw_diamond():
-    glBegin(GL_QUADS)
+    glBegin(GL_POLYGON)
     c = 0
     for edge in edges:
         for vertex in edge:
@@ -52,7 +52,15 @@ def draw_floor():
         glVertex3fv((-10, 0, z))
     glEnd()
     
-
+def draw_sphere():
+    glPushMatrix()
+    glTranslate(5, 5, -3)
+    glColor((0.2, 1, 0.4))
+    glutSolidSphere(1, 10, 20)
+    glTranslate(2, 2, -2)
+    glColor((0.3, 0.3, 0.8))
+    glutWireSphere(3, 10, 30)
+    glPopMatrix()
 
 
 def main():
@@ -61,19 +69,15 @@ def main():
     pygame.init()
     display = (800, 800)
     screen = pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    
+    glutInit()
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(90, 1, 0.1, 100)
     
-    x = 0.0
-    y = 0.0
-    z = 5.0
-    # glTranslatef(x, y, z)
-    #print_mv()
-    # gluLookAt(0, 3, 5, 0, 0, 0, 0, 1, 0)
-    #print_mv()
- 
+    x = 10.0
+    y = 40.0
+    z = 45.0
+
     rotation_direction = 0
     quit = False
     while not quit:
@@ -95,15 +99,17 @@ def main():
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        gluLookAt(0, y, z, 0, 0, 0, 0, 1, 0)
-        z += 0.1
-        y += 0.1
+        gluLookAt(0, y, z, 0, 0, 0, x, 1, 0)
+        z -= 0.1
+        y -= 0.1
+        x -= 0.05
  
         draw_diamond()
         draw_floor()
+        draw_sphere()
 
         pygame.display.flip()
-        pygame.time.wait(10)
+        # pygame.time.wait(10)
 
     pygame.display.quit()
     pygame.quit()
