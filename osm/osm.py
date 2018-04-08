595b677458c262bab2d3fc1ea75a9e18cce8c5cb
See-Mong Tan <seemong@gmail.com>
2018-04-08 10:42:11 -0700

    hide internal methods

--- osm/osm.py
+++ osm/osm.py
@@ -7,7 +7,7 @@ import time
 import argparse
 from geometry.mapobject import *
 
-def make_osm_node(elem):
+def _make_osm_node(elem):
     """
     Make a Node out of the OSM XML element
     """
@@ -18,7 +18,7 @@ def make_osm_node(elem):
     
     return Node(id, (lon, lat, 0))
     
-def make_osm_way(elem):
+def _make_osm_way(elem):
     """
     Make a Way out of the OSM XML tree. 
     Update all nodes to have the altitude if it's an elevation 
@@ -48,7 +48,7 @@ def make_osm_way(elem):
         
     return w
 
-def make_osm_map(osmf):
+def make_osm_map(name, osmf):
     """
     Given the OSM file osmf, read the file and make a Map out of it.
     Implement XML reading in a memory efficient manner.
@@ -62,10 +62,12 @@ def make_osm_map(osmf):
     # get the root
     event, root = context.next()
 
-    # The map we will return, together with a function that
-    # will return the node in the map given an id
-    m = Map('The Map')
+    # The map we will return
+    m = Map(name)
     
+    # save flag determines whether or not to save the XML tree
+    # in memory. If it's not set, the tree and elements are freed
+    # at each iteration, in order to save on memory
     save = False
     for event, elem in context:
         if event == 'start' and \
@@ -73,12 +75,12 @@ def make_osm_map(osmf):
             # save tree elements until we see the end
             save = True
         elif event == 'end' and elem.tag == 'node':
-            node = make_osm_node(elem)
+            node = _make_osm_node(elem)
             m.add_node(node)
             save = False
         elif event == 'end' and elem.tag == 'way':
             # tree = etree.ElementTree(elem)
-            way = make_osm_way(elem)
+            way = _make_osm_way(elem)
             m.add_way(way)
             save = False
 
@@ -92,7 +94,8 @@ def make_osm_map(osmf):
     return m
 
 if __name__ == "__main__":
-    m = make_osm_map(sys.argv[1])
+    assert len(sys.argv) == 2
+    m = make_osm_map("The Map", sys.argv[1])
     ways = m.get_all_ways()
     for w in ways:
         print(w.get_id(), m.get_node_indices_for_way(w))
